c80973fc02ac31b2d9b939130e57d35b9951813b
blueslimee <blueslimexdfodeu@gmail.com>
2021-03-11 13:24:07 -0400

    add androidlibz.a for armv8l

--- native/cgo.go
+++ native/cgo.go
@@ -3,7 +3,8 @@ package native
 /*
 #cgo CFLAGS: -I${SRCDIR}/zlib/
 #cgo windows LDFLAGS: ${SRCDIR}/libs/winlibz.a
-#cgo linux LDFLAGS: ${SRCDIR}/libs/linuxlibz.a
+#cgo !android,linux LDFLAGS: ${SRCDIR}/libs/linuxlibz.a
 #cgo darwin LDFLAGS: ${SRCDIR}/libs/darwinlibz.a
+#cgo android LDFLAGS: ${SRCDIR}/libs/androidlibz.a
 */
 import "C"
